d8eec919f88a42115f70950b10f3d91f6b784824
Muhamad Faidil Aziz <158293098+fadilaziz@users.noreply.github.com>
2024-12-22 01:09:28 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        std: ["Poppins", "Poppins"],
+        alt: ["Poppins", "Poppins"],
+      }
+    },
+  },
+  plugins: [],
+}
